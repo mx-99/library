4560a3524fd7029b7a521cbe4504cd860b569186
mx-99 <125824465+mx-99@users.noreply.github.com>
2026-02-28 10:02:10 +0300

    Add eventlisteners
    readstatus prototype, toggleRead, removeBook, clearForm and more

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 let myLibrary = [
-  {
-    id: "a6bc2b3f-060c-40b2-91dd-1743d3b91888",
-    title: "Introduction to the Algorithms",
-    author: "Thomas H. Cormen",
-    pages: 1312,
-    read: false
-  }
+    new Book(crypto.randomUUID(), "The Pragmatic Programmer", "Andrew Hunt & David Thomas", 352, true),
+    new Book(crypto.randomUUID(), "You Don’t Know JS Yet", "Kyle Simpson", 143, false),
+    new Book(crypto.randomUUID(), "Introduction to Algorithms", "Thomas H. Cormen", 1312, false),
 ];
 
 function Book(id, title, author, pages, read) {
@@ -16,31 +12,72 @@ function Book(id, title, author, pages, read) {
   this.read = read
 }
 
+Book.prototype.toggleReadStatus = function(){
+  this.read = !this.read;
+}
+
 function addBookToLibrary() {
   let id = crypto.randomUUID();
   let title = document.querySelector('#title').value;
   let author = document.querySelector('#author').value;
   let pages = document.querySelector('#pages').value;
   let read = document.querySelector('#read').checked;
-  console.log
   let book = new Book(id, title, author, pages, read);
   myLibrary.push(book);
   displayBooks();
+  clearForm();
 }
 
 function displayBooks() {
-  const books = document.querySelector('.container');
-  books.innerHTML = '';
-  myLibrary.forEach((obj) => {
-    const book_cards = document.createElement('div');
-    book_cards.className = 'cards';
-    book_cards.innerHTML = `
-    <h3>Id:= ${obj.id}<br> Title:= ${obj.title}<br> Author:= ${obj.author}<br>
-    Pages:= ${obj.pages}<br> ${obj.read ? "Read" : "Not Read"}</h3>`;
-    books.appendChild(book_cards)
-  })
+  const books = document.querySelector('.container')
+  books.innerHTML = "";
+  myLibrary.forEach((obj, index) => {
+      const book_cards = document.createElement('div')
+      book_cards.className = 'cards'
+      book_cards.innerHTML = `
+      <h3>${obj.title} <br> By: ${obj.author}</h3>
+      <p>${obj.pages} pages ${obj.read ? "Read" : "Not Read"}</p>
+      <button data-id="${obj.id}" class="toggle-read">Read Status </button>
+      <button data-id="${obj.id}" class="remove-book">Remove </button>`;
+      books.appendChild(book_cards)
+  });
+  attachEventListeners()
 }
 
+function attachEventListeners(){
+  const toggleReadBtns = document.querySelectorAll('.toggle-read');
+  const removeBtns = document.querySelectorAll('.remove-book');
+
+  toggleReadBtns.forEach((button) =>{
+    button.addEventListener('click', toggleRead);
+  })
+
+  removeBtns.forEach((button) =>{
+    button.addEventListener('click', removeBook);
+  })
+
+}
+
+function toggleRead(e){
+  const id = e.target.dataset.id;
+  const book = myLibrary.find(book => book.id === id);
+  console.log(id)
+  book.toggleReadStatus();
+  displayBooks();
+}
+
+function removeBook(e){
+  const id = e.target.dataset.id;
+  myLibrary = myLibrary.filter(book => book.id !== id);
+  displayBooks();
+}
+
+function clearForm() {
+  document.getElementById("title").value = '';
+  document.getElementById("author").value = '';
+  document.getElementById("pages").value = '';
+  document.getElementById("read").checked = false;
+}
 
 const addBtn = document.querySelector('#show-dialog');
 const dialog = document.querySelector('#my-dialog');
